1a6e54db16a0c97aa9d5c567c6dfcab456125f85
Sikhumbuzo <50705022+sssithole@users.noreply.github.com>
2022-09-01 15:39:58 +0200

    localhost:8080

---
+++ postpix/src/main/java/com/postpix/postpix/PostPics.java
@@ -0,0 +1,21 @@
+package com.postpix.postpix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+@EnableAutoConfiguration
+public class PostPics {
+    @RequestMapping("/")
+    @ResponseBody
+    String home(){
+        return "Hello World";
+    }
+    public static void main(String[] args){
+        SpringApplication.run(PostPics.class, args);
+    }
+}
